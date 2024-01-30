43bfb9c690f8b5053b41977e517d1ce88d1e6d3c
Barsi Balázs <balazs.barsi@stylersonline.com>
2024-01-30 13:31:49 +0100

    iDS-007 cypress test

--- cypress/e2e/DemoButton.cy.ts
+++ cypress/e2e/DemoButton.cy.ts
@@ -36,33 +36,56 @@ describe('ids Button Demo', () => {
     success: "rgb(34, 197, 94)",
     warning: "rgb(249, 115, 22)",
     light: "rgb(255, 255, 255)",
-    dark: "rgb(15, 23, 42)",
+    dark: "rgb(2, 6, 23)",
+  }];
+
+  const allWidth = [{
+    compact: "24px",
+    comfortable: "40px",
+    spacious: "56px",
   }]
 
   const white = 'rgba(255, 255, 255, 0)';
 
   it('Checks the content of button', () => {
-    cy.visit('/demo')
+    cy.visit('/demo');
 
     allModes.forEach((mode) => {
       allSizes.forEach((size) => {
         allVariants.forEach((variant) => {
-          allBgColors.forEach((color) => {
+          allWidth.forEach((width) => {
             const buttonSelector = `#${mode}-${variant}-${size}-button`;
             cy.get(buttonSelector).should('be.visible').contains(`${mode} ${variant} ${size} button`);
-            if (mode === 'outlined') {
-              cy.get(buttonSelector).trigger('mouseover').should('be.visible')
-                .should('have.css', 'background-color').and('eq', white)
-              cy.get(buttonSelector).trigger('mouseover').should('be.visible')
-                .should('have.css', 'color').and('eq', color[variant]);
-            } else {
-              cy.get(buttonSelector).trigger('mouseover').should('be.visible')
-                .should('have.css', 'background-color').and('eq', color[variant]);
-            }
+            cy.get(buttonSelector).should('be.visible').should('have.css', 'height').and('eq', width[size]);
           });
         });
       });
     });
   });
+
+  it('Checks the color of button', () => {
+    cy.visit('/demo')
+    allModes.forEach((mode) => {
+      allSizes.forEach((size) => {
+        allVariants.forEach((variant) => {
+          allBgColors.forEach((bgColors) => {
+            allTextColors.forEach((color) => {
+              const buttonSelector = `#${mode}-${variant}-${size}-button`;
+              if (mode === 'outlined' || mode === 'text') {
+                cy.get(buttonSelector).trigger('mouseover').should('be.visible')
+                  .should('have.css', 'background-color').and('eq', white)
+                cy.get(buttonSelector).trigger('mouseover').should('be.visible')
+                  .should('have.css', 'color').and('eq', color[variant]);
+              } else {
+                cy.get(buttonSelector).trigger('mouseover').should('be.visible')
+                  .should('have.css', 'background-color').and('eq', bgColors[variant]);
+              }
+            });
+          });
+        });
+      });
+    });
+
+  })
 
 });
