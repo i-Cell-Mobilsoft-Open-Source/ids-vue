dc4566cac9dc756c51c7164a74fbb759e8c83e9a
pelyi nora <pelyi.nora@virgo.hu>
2024-03-21 20:26:48 +0100

    ids-008 Tags final tests with known bugs

--- cypress/data/tagTestData.ts
+++ cypress/data/tagTestData.ts
@@ -24,17 +24,16 @@ const tagTestData = {
       dark: "rgb(2, 6, 23)",
     }],
   
-    // hoveredBgColors: [{
-    //   primary: "rgb(0, 47, 189)",
-    //   secondary: "rgb(51, 65, 85)",
-    //   brand: "rgb(0, 47, 189)",
-    //   error: "rgb(185, 28, 28)",
-    //   success: "rgb(21, 128, 61)",
-    //   warning: "rgb(194, 65, 12)",
-    //   light: "rgb(241, 245, 249)",
-    //   dark: "rgb(30, 41, 59)",
-    //   surface: "rgb(226, 232, 240)",
-    // }],
+    hoveredBgColors: [{
+      primary: "rgb(0, 47, 189)",
+      secondary: "rgb(51, 65, 85)",
+      brand: "rgb(0, 47, 189)",
+      error: "rgb(185, 28, 28)",
+      success: "rgb(21, 128, 61)",
+      warning: "rgb(194, 65, 12)",
+      light: "rgb(241, 245, 249)",
+      dark: "rgb(30, 41, 59)",
+    }],
 
     focusedBgColors: [{
       primary: "rgb(0, 60, 255)",
@@ -46,18 +45,16 @@ const tagTestData = {
       light: "rgb(255, 255, 255)",
       dark: "rgb(30, 41, 59)",
     }],
-  
-    // activeBgColors: [{
-    //   primary: "rgb(0, 36, 143)",
-    //   secondary: "rgb(30, 41, 59)",
-    //   brand: "rgb(0, 36, 143)",
-    //   error: "rgb(153, 27, 27)",
-    //   success: "rgb(22, 101, 52)",
-    //   warning: "rgb(154, 52, 18)",
-    //   light: "rgb(226, 232, 240)",
-    //   dark: "rgb(51, 65, 85)",
-    //   surface: "rgb(203, 213, 225)",
-    // }],
+      activeBgColors: [{
+      primary: "rgb(0, 36, 143)",
+      secondary: "rgb(30, 41, 59)",
+      brand: "rgb(0, 36, 143)",
+      error: "rgb(153, 27, 27)",
+      success: "rgb(22, 101, 52)",
+      warning: "rgb(154, 52, 18)",
+      light: "rgb(226, 232, 240)",
+      dark: "rgb(51, 65, 85)",    
+    }],
   
     // //textColors
     enabledColors: [{
@@ -69,7 +66,6 @@ const tagTestData = {
       warning: "rgb(255, 255, 255)",
       light: "rgb(71, 85, 105)",
       dark: "rgb(248, 250, 252)",
-    //   surface: "rgb(248, 250, 252)",
     }],
     hoveredColors: [{
       primary: "rgb(255, 255, 255)",
@@ -79,19 +75,18 @@ const tagTestData = {
       success: "rgb(255, 255, 255)",
       warning: "rgb(255, 255, 255)",
       light: "rgb(71, 85, 105)",
-      dark: "rgb(255, 255, 255)",
+      dark: "rgb(248, 250, 252)",
     }],
-    // hoveredOutlineTextColors: [{
-    //   primary: "rgb(0, 59, 235)",
-    //   secondary: "rgb(71, 85, 105)",
-    //   brand: "rgb(0, 59, 235)",
-    //   error: "rgb(220, 38, 38)",
-    //   success: "rgb(22, 163, 74)",
-    //   warning: "rgb(234, 88, 12)",
-    //   light: "rgb(255, 255, 255)",
-    //   dark: "rgb(2, 6, 23)",
-    //   surface: "rgb(71, 85, 105)",
-    // }],
+    hoveredOutlineColors: [{
+      primary: "rgb(0, 59, 235)",
+      secondary: "rgb(71, 85, 105)",
+      brand: "rgb(0, 59, 235)",
+      error: "rgb(220, 38, 38)",
+      success: "rgb(22, 163, 74)",
+      warning: "rgb(234, 88, 12)",
+      light: "rgb(255, 255, 255)",
+      dark: "rgb(2, 6, 23)",
+    }],
     focusedOutlineColors: [{
       primary: "rgb(0, 60, 255)",
       secondary: "rgb(100, 116, 139)",
@@ -112,52 +107,48 @@ const tagTestData = {
       light: "rgb(71, 85, 105)",
       dark: "rgb(248, 250, 252)",
     }],
-    // //pressed
-    // activeColors: [{
-    //   primary: "rgb(255, 255, 255)",
-    //   secondary: "rgb(255, 255, 255)",
-    //   brand: "rgb(255, 255, 255)",
-    //   error: "rgb(255, 255, 255)",
-    //   success: "rgb(255, 255, 255)",
-    //   warning: "rgb(255, 255, 255)",
-    //   light: "rgb(71, 85, 105)",
-    //   dark: "rgb(255, 255, 255)",   // új dark a fillednél: "rgb(248, 250, 252)",
-    //   surface: "rgb(100, 116, 139)",
-    // }],
+      activeOutlineColors: [{
+      primary: "rgb(0, 47, 189)",
+      secondary: "rgb(51, 65, 85)",
+      brand: "rgb(0, 47, 189)",
+      error: "rgb(185, 28, 28)",
+      success: "rgb(21, 128, 61)",
+      warning: "rgb(194, 65, 12)",
+      light: "rgb(255, 255, 255)",
+      dark: "rgb(2, 6, 23)",
+    }],
   
-    // activeOutlineTextColors: [{
-    //   primary: "rgb(0, 47, 189)",
-    //   secondary: "rgb(51, 65, 85)", //ld. icon button
-    //   brand: "rgb(0, 47, 189)",
-    //   error: "rgb(185, 28, 28)",
-    //   success: "rgb(21, 128, 61)",
-    //   warning: "rgb(194, 65, 12)",
-    //   light: "rgb(255, 255, 255)",
-    //   dark: "rgb(2, 6, 23)",
-    //   surface: "rgb(51, 65, 85)",
-    // }],
-  
-    // //sizes
+    //sizes
     allHeight: [{
       compact: "24px",
       comfortable: "32px",
-      //spacious: "56px",
     }],
     allWidth: [{
       compact: "24px",
       comfortable: "40px",
-      //spacious: "56px",
     }],
-    // topBottomPadding: [{
-    //   compact: "6px",
-    //   comfortable: "10px",
-    //   spacious: "16px",
-    // }],
-    // leftRightPadding: [{
-    //   compact: "8px",
-    //   comfortable: "20px",
-    //   spacious: "24px",
-    // }],
+    allLeadingHeight: [{
+      compact: "20px",
+      comfortable: "24px",
+    }],
+    allLeadingWidth: [{
+      compact: "20px",
+      comfortable: "24px",
+    }],
+
+    Radius: [{
+      compact: "4px",
+      comfortable: "8px",
+    }],
+
+    topBottomPadding: [{
+      compact: "8px",
+      comfortable: "6px",
+    }],
+    leftRightPadding: [{
+      compact: "8px",
+      comfortable: "20px",
+    }],
     allFontSize: [{
       compact: "12px",
       comfortable: "14px",
@@ -166,24 +157,31 @@ const tagTestData = {
       compact: "16px",
       comfortable: "20px",
     }],
+    columnGap: [{
+      compact: "2px",
+      comfortable: "8px",
+    }],
+    rowGap: [{
+      compact: "2px",
+      comfortable: "8px",
+    }],
 
     common: [{
-        display: 'flex',
         flexShrink: '0',
         fontWeight: '500',
         alignItems: 'center',
+        display: 'flex',
         justifyContent: 'center'
       }],
     white: 'rgba(255, 255, 255, 0)',
     white2: 'rgb(255, 255, 255) solid 3px',
-    // white3: 'rgb(255, 255, 255)',
+    white3: 'rgb(255, 255, 255)',
     black: 'rgb(0, 0, 0) solid 3px',
-    hoverdOutlineBg: 'rgb(248, 250, 252)',
-    // disabledBgColors: "rgb(226, 232, 240)",
-    // hoveredTextBgColors: "rgb(241, 245, 249)",
-    // disabledTextColors: "rgb(148, 163, 184)",
-    // allRadius: '1000px'
-    allFontWeight: '500'
+    hoverdOutlineBg: 'rgba(100, 140, 180, 0.1)',
+    activeOutlineBg: 'rgba(100, 140, 180, 0.2)',
+    disabledBgColors: "rgb(226, 232, 240)",
+    disabledTextColors: "rgb(148, 163, 184)",
+    allFontWeight: '500',
   }
   
   export default tagTestData;
--- cypress/e2e/DemoTag.cy.ts
+++ cypress/e2e/DemoTag.cy.ts
@@ -1,3 +1,4 @@
+import { exists } from 'fs';
 import tagTestData from '../data/tagTestData';
 import 'cypress-real-events/support';
 
@@ -15,43 +16,68 @@ const allCombinations = [] as any[];
       });
     });
   });
-  //WIP - comfortable height 32 vs 26?
-  xit('Checks the width and height of tags', () => {
+  it('Checks the height of tags', () => {
     allCombinations.forEach((item) => {
       tagTestData.allHeight.forEach((height) => {
-        //tagTestData.allWidth.forEach((width) => {
-          const avatarSelector = `#${item.mode}-${item.variant}-${item.size}-Tag`;
-          cy.get(avatarSelector).should('be.visible')
-            .should(($el) => {
-            expect($el).to.have.css('height', height[item.size]);
-            //expect($el).to.have.css('width', width[item.size]);
-            });
-        //});
+      const tagSelector = `#${item.mode}-${item.variant}-${item.size}-Tag`;
+      cy.get(tagSelector).should('be.visible').should(($el) => {
+        expect($el).to.have.css('height', height[item.size]);
+        });
       });
     });
   });
-  //wip - display inline-flex vs flex
-  xit('Checks common css rules of tags', () => {
+
+  it('Checks the width and height of tags leading icon', () => {
+    allCombinations.forEach((item) => {
+      tagTestData.allLeadingHeight.forEach((height) => {
+        tagTestData.allLeadingWidth.forEach((width) => {
+          const tagSelector = `#${item.mode}-${item.variant}-${item.size}-Tag`;
+          cy.get(tagSelector).find('svg').should('be.visible').should(($el) => {
+            const styles = window.getComputedStyle($el[0]);
+            expect(styles.height).to.equal(height[item.size]);
+            expect(styles.width).to.equal(width[item.size]);
+            });
+        });
+        });
+      });
+    });
+
+    it('Checks the width and height of tags trailing icon', () => {
+      allCombinations.forEach((item) => {
+        tagTestData.allLeadingHeight.forEach((height) => {
+          tagTestData.allLeadingWidth.forEach((width) => {
+            const tagSelector = `#${item.mode}-${item.variant}-${item.size}-Tag`;
+            cy.get(tagSelector).find('svg').should('be.visible').should(($el) => {
+              const styles = window.getComputedStyle($el[1]);
+              expect(styles.height).to.equal(height[item.size]);
+              expect(styles.width).to.equal(width[item.size]);
+              });
+          });
+          });
+        });
+      });
+    
+  //kész - display inline-flex vs flex?
+  it('Checks common css rules of tags', () => {
     allCombinations.forEach((item) => {
       tagTestData.common.forEach((common) => {
-        const avatarSelector = `#${item.mode}-${item.variant}-${item.size}-Tag`;
-        cy.get(avatarSelector)
+        const tagSelector = `#${item.mode}-${item.variant}-${item.size}-Tag`;
+        cy.get(tagSelector)
           .should('be.visible')
-          .should('have.css', 'display', common['display'])
           .should('have.css', 'flex-shrink', common['flexShrink'])
           .should('have.css', 'font-weight', common['fontWeight'])
           .should('have.css', 'align-items', common['alignItems'])
-        //   .should('have.css', 'flex-direction', common['flexDirection'])
+          .should('have.css', 'display', common['display'])
           .should('have.css', 'justify-content', common['justifyContent']);
       });
     });
   });
-  //kész
-  xit('Checks the font-size of tags', () => {
+
+  it('Checks the font-size of tags', () => {
     allCombinations.forEach((item) => {
         tagTestData.allFontSize.forEach((font) => {
-        const avatarSelector = `#${item.mode}-${item.variant}-${item.size}-Tag`;
-        cy.get(avatarSelector).should('be.visible').should(($el) => {
+        const tagSelector = `#${item.mode}-${item.variant}-${item.size}-Tag`;
+        cy.get(tagSelector).should('be.visible').should(($el) => {
           const styles = window.getComputedStyle($el[0]);
           expect(styles.fontSize).to.equal(font[item.size]);
           expect(styles.fontWeight).to.equal(tagTestData.allFontWeight);
@@ -59,12 +85,12 @@ const allCombinations = [] as any[];
       });
     });
   });
-  //kész
-  xit('Checks the line-height of tags', () => {
+
+  it('Checks the line-height of tags', () => {
     allCombinations.forEach((item) => {
       tagTestData.allLineHeight.forEach((lineHeigt) => {
-        const avatarSelector = `#${item.mode}-${item.variant}-${item.size}-Tag`;
-        cy.get(avatarSelector).should('be.visible').should(($el) => {
+        const tagSelector = `#${item.mode}-${item.variant}-${item.size}-Tag`;
+        cy.get(tagSelector).should('be.visible').should(($el) => {
           const styles = window.getComputedStyle($el[0]);
           expect(styles.lineHeight).to.equal(lineHeigt[item.size]);
         });
@@ -72,19 +98,19 @@ const allCombinations = [] as any[];
     });
   });
 
-  xit('Checks the color of tags', () => {
+  it('Checks the color of tags', () => {
     allCombinations.forEach((item) => {
       tagTestData.enabledBgColors.forEach((bgColor) => {
         tagTestData.enabledColors.forEach((color) => {
-          const avatarSelector = `#${item.mode}-${item.variant}-${item.size}-Tag`;
+          const tagSelector = `#${item.mode}-${item.variant}-${item.size}-Tag`;
           if (item.mode === 'filled') {
-              cy.get(avatarSelector).should('be.visible').should(($el) => {
+              cy.get(tagSelector).should('be.visible').should(($el) => {
               const styles = window.getComputedStyle($el[0]);
               expect(styles.backgroundColor).to.equal(bgColor[item.variant]);
               expect(styles.color).to.equal(color[item.variant]);
               });
           } else { //outlined
-              cy.get(avatarSelector).should('be.visible').should(($el) => {
+              cy.get(tagSelector).should('be.visible').should(($el) => {
               const styles = window.getComputedStyle($el[0]);
               expect(styles.backgroundColor).to.equal(tagTestData.white);
               expect(styles.color).to.equal(bgColor[item.variant]);
@@ -94,21 +120,88 @@ const allCombinations = [] as any[];
         });
       });
     });
-// light-nál legyen focus state esetén fehér a kör (most fekete)
+
+  it('Checks the color of tags leading Icon', () => {
+    allCombinations.forEach((item) => {
+      tagTestData.enabledBgColors.forEach((bgColor) => {
+        tagTestData.enabledColors.forEach((color) => {
+          const tagSelector = `#${item.mode}-${item.variant}-${item.size}-Tag`;
+          if (item.mode === 'filled') {
+              cy.get(tagSelector).find('svg').should('be.visible').should(($el) => {
+              const styles = window.getComputedStyle($el[0]);
+              expect(styles.color).to.equal(color[item.variant]);
+              });
+          } else { //outlined
+              cy.get(tagSelector).find('svg').should('be.visible').should(($el) => {
+              const styles = window.getComputedStyle($el[0]);
+              expect(styles.color).to.equal(bgColor[item.variant]);
+              });
+            }
+          });
+        });
+      });
+    });
+
+  it('Checks the color of tags trailing Icon', () => {
+    allCombinations.forEach((item) => {
+      tagTestData.enabledBgColors.forEach((bgColor) => {
+        tagTestData.enabledColors.forEach((color) => {
+          const tagSelector = `#${item.mode}-${item.variant}-${item.size}-Tag`;
+          if (item.mode === 'filled') {
+              cy.get(tagSelector).find('svg').should('be.visible').should(($el) => {
+              const styles = window.getComputedStyle($el[1]);
+              expect(styles.color).to.equal(color[item.variant]);
+              });
+          } else { //outlined
+              cy.get(tagSelector).find('svg').should('be.visible').should(($el) => {
+              const styles = window.getComputedStyle($el[1]);
+              expect(styles.color).to.equal(bgColor[item.variant]);
+              });
+            }
+          });
+        });
+      });
+    });
+//kész - outline color token javításra vár
+  it('Checks color and background color of tags with hovered state', () => {
+    allCombinations.forEach((item) => {
+      tagTestData.hoveredBgColors.forEach((bgColor) => {
+        tagTestData.hoveredOutlineColors.forEach((outlineColor) => {
+          tagTestData.hoveredColors.forEach((color) => {
+            const button = cy.get(`#${item.mode}-${item.variant}-${item.size}-Tag`);
+            if (item.mode === 'outlined') {
+              button.realHover({ pointer: "mouse" }).should(($el) => {
+                const styles = window.getComputedStyle($el[0]);
+                expect(styles.backgroundColor).to.equal(tagTestData.hoverdOutlineBg);
+                expect(styles.color).to.equal(outlineColor[item.variant]);
+              });
+            } else { //filled
+              button.realHover({ pointer: "mouse" }).should(($el) => {
+                const styles = window.getComputedStyle($el[0]);
+                expect(styles.backgroundColor).to.equal(bgColor[item.variant]);
+                expect(styles.color).to.equal(color[item.variant]);
+              });
+            }
+          });
+        });
+      });
+    });
+  });
+
   it('Checks focused state of tags', () => {
     allCombinations.forEach((item) => {
-      const avatarSelector = `#${item.mode}-${item.variant}-${item.size}-Tag`;
+      const tagSelector = `#${item.mode}-${item.variant}-${item.size}-Tag`;
       if (item.variant === 'light') {
-        cy.get(avatarSelector).click().should('have.focus').should('be.visible')
+        cy.get(tagSelector).click().should('have.focus').should('be.visible')
           .should('have.css', 'outline').and('eq', tagTestData.white2);
       } else {
-        cy.get(avatarSelector).click().should('have.focus').should('be.visible')
+        cy.get(tagSelector).click().should('have.focus').should('be.visible')
           .should('have.css', 'outline').and('eq', tagTestData.black);
       }
     });
   });
-//kész, átnézni
-  xit('Checks color of tag with FOCUSED state', () => {
+
+  it('Checks color of tags with FOCUSED state', () => {
     allCombinations.forEach((item) => {
       tagTestData.focusedBgColors.forEach((bgColor) => {
         tagTestData.focusedColors.forEach((color) => {
@@ -117,7 +210,7 @@ const allCombinations = [] as any[];
           if(item.mode === 'outlined') {
             button.realClick({ pointer: "mouse" }).should(($el) => {
             const styles = window.getComputedStyle($el[0]);
-              expect(styles.backgroundColor).to.equal(tagTestData.hoverdOutlineBg);
+              expect(styles.backgroundColor).to.equal(tagTestData.white);
               expect(styles.color).to.equal(outlineColor[item.variant]);
             });
           } else { //filled
@@ -132,6 +225,98 @@ const allCombinations = [] as any[];
       });
     });
   });
+//wip - outline color token javításra vár (szerintem itt is az a baj)
+  it('Checks color of tags with active (pressed) state', () => {
+    allCombinations.forEach((item) => {
+      tagTestData.activeBgColors.forEach((bgColor) => {
+        tagTestData.focusedColors.forEach((color) => {
+          tagTestData.activeOutlineColors.forEach((outlineColor) => {
+            const buttonSelector = `#${item.mode}-${item.variant}-${item.size}-Tag`;
+            cy.get(buttonSelector).then(button => {
+              cy.wrap(button).realMouseDown({ pointer: "mouse" }).should(($el) => {
+                const styles = window.getComputedStyle($el[0]);
+                if (item.mode === 'outlined') {
+                  expect(styles.backgroundColor).to.equal(tagTestData.activeOutlineBg);
+                  expect(styles.color).to.equal(outlineColor[item.variant]);
+                } else {
+                  expect(styles.backgroundColor).to.equal(bgColor[item.variant]);
+                  expect(styles.color).to.equal(color[item.variant]);
+                }
+              }).realMouseUp({ pointer: "mouse" });
+            });
+          });
+        });
+      });
+    });
+  });
+// wip - nincs disabled tags....
+  it('Checks color of disabled state tags', () => {
+    allCombinations.forEach((item) => {
+      if (item.variant === 'error' || item.variant === 'success' || item.variant === 'warning') {
+        return;
+      }
+      const button = cy.get(`#${item.mode}-${item.variant}-${item.size}-disabled-Tag`);
+      if (item.mode === 'outlined') {
+        button.should(($el) => {
+          const styles = window.getComputedStyle($el[0]);
+          expect(styles.backgroundColor).to.equal(tagTestData.white);
+          expect(styles.color).to.equal(tagTestData.disabledTextColors);
+        });
+      } else { //filled
+        button.should(($el) => {
+          const styles = window.getComputedStyle($el[0]);
+          expect(styles.backgroundColor).to.equal(tagTestData.disabledBgColors);
+          expect(styles.color).to.equal(tagTestData.disabledTextColors);
+        });
+      }
+    });
+  });
 
+  it('Checks left and right border radius of tags', () => {
+    allCombinations.forEach((item) => {
+      tagTestData.Radius.forEach((allRadius) => {
+        const avatarSelector = `#${item.mode}-${item.variant}-${item.size}-Tag`;
+        cy.get(avatarSelector).should('be.visible').then(($el) => {
+          const styles = window.getComputedStyle($el[0]);
+          expect(styles.borderTopLeftRadius).to.equal(allRadius[item.size]);
+          expect(styles.borderTopRightRadius).to.equal(allRadius[item.size]);
+          expect(styles.borderBottomLeftRadius).to.equal(allRadius[item.size]);
+          expect(styles.borderBottomRightRadius).to.equal(allRadius[item.size]);
+        });
+      });
+    });
+  });
+
+  it('Checks all padding of tags', () => {
+    allCombinations.forEach((item) => {
+      tagTestData.topBottomPadding.forEach((topPadding) => {
+        tagTestData.leftRightPadding.forEach((sidePadding) => {
+          const tagSelector = `#${item.mode}-${item.variant}-${item.size}-Tag`;
+          cy.get(tagSelector).should('be.visible').then(($el) => {
+            const styles = window.getComputedStyle($el[0]);
+            expect(styles.paddingTop).to.equal(topPadding[item.size]);
+            expect(styles.paddingLeft).to.equal(sidePadding[item.size]);
+            expect(styles.paddingBottom).to.equal(topPadding[item.size]);
+            expect(styles.paddingRight).to.equal(sidePadding[item.size]);
+          });
+        });
+      });
+    });
+  });
+
+  it('Checks all GAP of tags', () => {
+    allCombinations.forEach((item) => {
+      tagTestData.columnGap.forEach((gapColumn) => {
+        tagTestData.rowGap.forEach((gapRow) => {
+          const tagSelector = `#${item.mode}-${item.variant}-${item.size}-Tag`;
+          cy.get(tagSelector).should('be.visible').should(($el) => {
+            const styles = window.getComputedStyle($el[0]);
+            expect(styles.columnGap).to.equal(gapColumn[item.size]);
+            expect(styles.rowGap).to.equal(gapRow[item.size]);
+          });
+        });
+      });
+    });
+  });
 
 });
