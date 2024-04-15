cf90b0d663b494d28d0beebf798ee9f578d9ac7c
Barsi Balázs <balazs.barsi@stylersonline.com>
2024-04-15 10:25:46 +0200

    ids-007 fixed shadow

--- cypress/data/actionPanelTestData.ts
+++ cypress/data/actionPanelTestData.ts
@@ -1,29 +1,29 @@
 const actionPanelTestData = {
-    allModes: ["filled", "outlined", "elevated"],
-    allSizes: ["compact"],
-    allVariants: ["light"],
+  allModes: ["filled", "outlined", "elevated"],
+  allSizes: ["compact"],
+  allVariants: ["light"],
 
   //common
-    display: 'flex',
-    width: '356px',
-    flexDirection: 'column',
-    alignItems: 'flex-start',
+  display: 'flex',
+  width: '356px',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
   //colors
-    enabledColors: 'rgb(33, 53, 71)',
-    white: 'rgb(255, 255, 255)',
-    filledBorderColor: 'rgb(229, 231, 235)',
-    outlinedBorderColor: 'rgb(203, 213, 225)',
-    elevatedBorderColor: 'rgb(229, 231, 235)',
+  enabledColors: 'rgb(33, 53, 71)',
+  white: 'rgb(255, 255, 255)',
+  filledBorderColor: 'rgb(229, 231, 235)',
+  outlinedBorderColor: 'rgb(203, 213, 225)',
+  elevatedBorderColor: 'rgb(229, 231, 235)',
   //sizes
   allWidth: [{
     compact: "356px",
   }],
-    radius: '16px',
-    padding: '8px',
-    gap: '8px',
-    filledBoxShadow: 'rgba(0, 0, 0, 0) 0px 0px 0px 0px, rgba(0, 0, 0, 0) 0px 0px 0px 0px',
-    outlinedBoxShadow: '0px 1px 2px 0px rgba(0, 0, 0, 0.05)',
-    elevatedBoxShadow: '0px 25px 50px -12px rgba(0, 0, 0, 0.25)',
+  radius: '16px',
+  padding: '8px',
+  gap: '8px',
+
+  outlinedBoxShadow: 'rgba(100, 140, 180, 0.05) 0px 1px 2px 0px',
+  elevatedBoxShadow: 'rgba(100, 140, 180, 0.25) 0px 25px 50px -12px',
 }
 
 export default actionPanelTestData;
