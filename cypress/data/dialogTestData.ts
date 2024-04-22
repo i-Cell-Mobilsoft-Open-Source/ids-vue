062bd6254448177e00be9c7eeb9b075dcfb20bf2
pelyi nora <pelyi.nora@virgo.hu>
2024-04-22 17:35:38 +0200

    ids-116 Dialog wip

--- cypress/data/dialogTestData.ts
+++ cypress/data/dialogTestData.ts
@@ -1,40 +1,41 @@
 const dialogTestData = {
 
   //Colors
-    enabledBgColors: "rgb(0, 60, 255)",
-    white: "rgb(255, 255, 255)",
-    panelWhite: 'rgba(255, 255, 255, 0)',
-    black: 'rgb(0, 0, 0)',
+  enabledBgColors: "rgb(0, 60, 255)",
+  white: "rgb(255, 255, 255)",
+  panelWhite: 'rgba(255, 255, 255, 0)',
+  black: 'rgb(0, 0, 0)',
 
-    //dialog size
-    width: '800px', //nem 600px?
-    gap: '24px',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'normal', //nem flex-start?
+  //dialog sizes
+  width: '800px', //nem 600px?
+  gap: '24px',
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'normal', //nem flex-start?
 
   //Label, Cancel, Save buttons sizes
-    minWidth: '40px',
-    height: '40px',
-    ButtonGap: '8px',
-    displayButton: 'inline-flex', //nem flex? vs a Cancel + Save miért csak flex?
-    fontWeight: '700',
-    fontSize: '14px',
-    lineHeight: '20px',
-    justifyContent: 'center',
-    alignButtonItems: 'center',
-    
-    buttonRadius: '1000px',
-    buttonTopBottomPadding: '10px',
-    buttonLeftRightPadding: '20px',
-    buttonGap: '8px',
-  
-    radius: '24px',
-    padding: '24px',
-    boxShadow: 'rgba(0, 0, 0, 0.25) 0px 25px 50px -12px',
+  minWidth: '40px',
+  height: '40px',
+  ButtonGap: '8px',
+  displayButton: 'inline-flex', //nem flex? vs a Cancel + Save miért csak flex?
+  fontWeight: '700',
+  fontSize: '14px',
+  lineHeight: '20px',
+  justifyContent: 'center',
+  alignButtonItems: 'center',
 
-    xWeightHeight: '24px',
-    xColor: 'rgb(71, 85, 105)'
-  }
-  
-  export default dialogTestData;
+  buttonRadius: '1000px',
+  buttonTopBottomPadding: '10px',
+  buttonLeftRightPadding: '20px',
+  buttonGap: '8px',
+
+  radius: '32px',
+  bottonTopPadding: '24px',
+  leftRightPadding: '0px',
+  boxShadow: 'rgba(0, 0, 0, 0.25) 0px 25px 50px -12px',
+
+  xWeightHeight: '40px',
+  xColor: 'rgb(71, 85, 105)'
+}
+
+export default dialogTestData;
