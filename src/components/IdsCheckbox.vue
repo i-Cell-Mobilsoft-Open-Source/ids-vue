7f63081a01265832999b8b1b0330eaaf2a7e74ab
Árpád Soós <arpad.soos@icellmobilsoft.hu>
2024-06-24 16:26:57 +0200

    [IDS]: checkbox style implementation (WIP)

--- src/components/IdsCheckbox.vue
+++ src/components/IdsCheckbox.vue
@@ -36,17 +36,29 @@ import { computed, reactive } from 'vue';
 			return props.indeterminate ? addClassPrefix('indeterminate') : null;
 		});
 
+
 		function addClassPrefix(className: string | null): string | null {
     	return className ? `${componentClass}-${className}` : null;
   	}
 </script>
 <template>
   <div :class="[state.classList, isDisabled, isValid, isIndeterminate]">
-    <input :id="props.id" type="checkbox" :disabled="props.disabled" :indeterminate="props.indeterminate">
-    <div class="ids-checkbox-label-wrapper">
-      <div class="ids-checkbox-label-container">
-        <label :for="props.id" class="ids-checkbox-label">Az Általános Szerződési Feltételeket a mai nappal tudomásul vettem és elfogadom</label>
-        <span class="ids-checkbox-asterisk">*</span>
+    <div class="ids-checkbox__input-wrapper">
+      <div class="ids-checkbox__touch-target" />
+      <input :id="props.id" type="checkbox" :disabled="props.disabled" :indeterminate="props.indeterminate">
+      <div class="ids-checkbox__icon">	
+        <!-- <svg class="ids-checkbox__checkmark" viewBox="0 0 20 20" focusable="false" fill="none" aria-hidden="true">
+          <path d="M3 10.5L9 16.5L18 3" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+        </svg> -->
+        <svg viewBox="0 0 20 20" focusable="false" aria-hidden="true">
+          <path d="M4 10H15.6667" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+        </svg>
+      </div>
+    </div>
+    <div class="ids-checkbox__label-wrapper">
+      <div class="ids-checkbox__label-container">
+        <label :for="props.id" class="ids-checkbox__label">Az Általános Szerződési Feltételeket a mai nappal tudomásul vettem és elfogadom</label>
+        <span class="ids-checkbox__asterisk">*</span>
       </div>
       <span class="ids-checkbox-hint">Checkbox: {{ props.variant }} - {{ props.size }}</span>
     </div>
@@ -64,19 +76,25 @@ $variants: light, dark, surface;
 	letter-spacing: var(--ids-comp-size-checkbox-message-typography-letter-spacing-#{$size});
 }
 
-
-@mixin checkmark-color($color) {
-	background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path fill="none" stroke="#{$color}" stroke-linecap="square" stroke-linejoin="square" stroke-width="1" d="M3,8 l3,3 l7-7"/></svg>');
-}
-
-@mixin indeterminate-marker-color($color) {
-	background-image: url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><path fill="none" stroke="#{$color}" stroke-linecap="square" stroke-linejoin="square" stroke-width="1" d="M4,8 h8"/></svg>');
-}
-
 .ids-checkbox {
 	display: inline-flex;
 	align-self: stretch;
 	align-items: flex-start;
+
+	&__input-wrapper {
+		position: relative;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+
+		.ids-checkbox__touch-target {
+			position: absolute;
+		}
+
+		.ids-checkbox__icon {
+			position: absolute;
+		}
+	}
 
   	@each $size in $sizes  {
 		&.ids-checkbox-#{$size} {
@@ -86,9 +104,6 @@ $variants: light, dark, surface;
 			input[type="checkbox"] {
 				appearance: none;
 				-webkit-appearance: none;
-				display: flex;
-				justify-content: center;
-				align-items: center;
 				flex-shrink: 0;
 				width: var(--ids-comp-size-checkbox-input-size-width-#{$size});
     			height: var(--ids-comp-size-checkbox-input-size-height-#{$size});
@@ -97,13 +112,28 @@ $variants: light, dark, surface;
 				border-style: solid;
 			}
 
+			.ids-checkbox__input-wrapper {
+				.ids-checkbox__touch-target {
+					top: calc((var(--ids-comp-size-checkbox-input-size-height-#{$size}) - 40px) / 2 );
+					left: calc((var(--ids-comp-size-checkbox-input-size-width-#{$size}) - 40px) / 2 );
+					height: 40px;
+					width: 40px;
+				}
+
+				.ids-checkbox__icon {
+					height: var(--ids-comp-size-checkbox-input-size-icon-#{$size});
+					width: var(--ids-comp-size-checkbox-input-size-icon-#{$size});
+				}
+			}
+
+
 			input[type="checkbox"]:focus {
 				outline-offset: 3px;
 				outline-style: solid;
 				outline-width: var(--ids-comp-size-checkbox-focused-outline-outline-#{size});
 			}
 
-			.ids-checkbox-label-wrapper {
+			.ids-checkbox__label-wrapper {
 				display: flex;
 				padding: var(--ids-comp-size-checkbox-label-group-size-padding-y-#{$size}) var(--ids-comp-size-checkbox-label-group-size-padding-x-#{$size});
 				flex-direction: column;
@@ -111,20 +141,20 @@ $variants: light, dark, surface;
 				gap: var(--ids-comp-size-checkbox-label-group-size-gap-#{$size});
 				flex: 1 0 0;
 				
-				.ids-checkbox-label-container {
+				.ids-checkbox__label-container {
 					@include commonMessageTypography($size);
 					display: flex;
 					font-size: var(--ids-comp-size-checkbox-label-typography-font-size-#{$size});
 					line-height: var(--ids-comp-size-checkbox-label-typography-line-height-#{$size});
 					
 
-					.ids-checkbox-label {
+					.ids-checkbox__label {
 						display: flex;
 						align-items: flex-start;
 						text-align: start;
 					}
 
-					.ids-checkbox-asterisk {
+					.ids-checkbox__asterisk {
 						color: var(--ids-comp-checkbox-asterisk-color-fg-enabled);
 						flex-shrink: 0;
 					}
@@ -141,21 +171,43 @@ $variants: light, dark, surface;
 
 	@each $variant in $variants {
 		&.ids-checkbox-#{$variant} {
+			.ids-checkbox__input-wrapper {
+				.ids-checkbox__icon {
+					color: var(--ids-comp-checkbox-input-selected-color-fg-icon-#{$variant}-enabled);
+
+					&:not(:disabled):hover {
+						color: var(--ids-comp-checkbox-input-selected-color-fg-icon-#{$variant}-hovered);
+					}
+
+					&:not(:disabled):active {
+						color: var(--ids-comp-checkbox-input-selected-color-fg-icon-#{$variant}-pressed);
+					}
+
+					&:not(:disabled):focus {
+						color: var(--ids-comp-checkbox-input-selected-color-fg-icon-#{$variant}-focused);
+					}
+
+					&:disabled {
+						color: var(--ids-comp-checkbox-input-selected-color-fg-icon-#{$variant}-disabled);
+					}
+				}
+			}
+
 			input[type="checkbox"] {
 				border-color: var(--ids-comp-checkbox-input-unselected-color-border-#{$variant}-enabled);
 				background-color: var(--ids-comp-checkbox-input-unselected-color-bg-#{$variant}-enabled);
 
-				&:hover {
+				&:not(:disabled):hover {
 					border-color: var(--ids-comp-checkbox-input-unselected-color-border-#{$variant}-hovered);
 					background-color: var(--ids-comp-checkbox-input-unselected-color-bg-#{$variant}-hovered);
 				}
 
-				&:active {
+				&:not(:disabled):active {
 					border-color: var(--ids-comp-checkbox-input-unselected-color-border-#{$variant}-pressed);
 					background-color: var(--ids-comp-checkbox-input-unselected-color-bg-#{$variant}-pressed);
 				}
 
-				&:focus {
+				&:not(:disabled):focus {
 					border-color: var(--ids-comp-checkbox-input-unselected-color-border-#{$variant}-focused);
 					background-color: var(--ids-comp-checkbox-input-unselected-color-bg-#{$variant}-focused);
 				}
@@ -167,23 +219,20 @@ $variants: light, dark, surface;
 			}
 
 			input[type="checkbox"]:checked, input[type="checkbox"]:indeterminate {
-				background-position: center;
-				background-repeat: no-repeat;
-				background-size: contain;
-				border-color: var(--ids-comp-checkbox-input-selected-color-border-#{$variant}-enabled);
-				background-color: var(--ids-comp-checkbox-input-selected-color-bg-#{$variant}-enabled);
+                border-color: var(--ids-comp-checkbox-input-selected-color-border-#{$variant}-enabled);
+                background-color: var(--ids-comp-checkbox-input-selected-color-bg-#{$variant}-enabled);
 
-				&:hover {
+				&:not(:disabled):hover {
 					border-color: var(--ids-comp-checkbox-input-selected-color-border-#{$variant}-hovered);
 					background-color: var(--ids-comp-checkbox-input-selected-color-bg-#{$variant}-hovered);
 				}
 
-				&:active {
+				&:not(:disabled):active {
 					border-color: var(--ids-comp-checkbox-input-selected-color-border-#{$variant}-pressed);
 					background-color: var(--ids-comp-checkbox-input-selected-color-bg-#{$variant}-pressed);
 				}
 
-				&:focus {
+				&:not(:disabled):focus {
 					border-color: var(--ids-comp-checkbox-input-selected-color-border-#{$variant}-focused);
 					background-color: var(--ids-comp-checkbox-input-selected-color-bg-#{$variant}-focused);
 				}
@@ -194,18 +243,18 @@ $variants: light, dark, surface;
 				}
 			}
 
-			.ids-checkbox-label {
+			.ids-checkbox__label {
 				color: var(--ids-comp-checkbox-label-color-fg-#{$variant}-enabled);
 
-				&:hover {
+				&:not(:disabled):hover {
 					color: var(--ids-comp-checkbox-label-color-fg-#{$variant}-hovered);
 				}
 
-				&:active {
+				&:not(:disabled):active {
 					color: var(--ids-comp-checkbox-label-color-fg-#{$variant}-pressed);
 				}
 
-				&:focus {
+				&:not(:disabled):focus {
 					color: var(--ids-comp-checkbox-label-color-fg-#{$variant}-focused);
 				}
 
@@ -222,23 +271,77 @@ $variants: light, dark, surface;
 				color: var(--ids-comp-checkbox-message-color-fg-text-error-enabled);
 			}
 
-			&.ids-checkbox-disabled .ids-checkbox-label {
+			&.ids-checkbox-disabled .ids-checkbox__label {
 				color: var(--ids-comp-checkbox-label-color-fg-#{$variant}-disabled);
 			}
-
-			input[type="checkbox"]:checked {
-				@include checkmark-color(white);
-			}
-
-			input[type="checkbox"]:indeterminate {
-				@include indeterminate-marker-color(white);
-			}
-
 		}
 	}
 
-	&.ids-checkbox-error {
+	&.ids-checkbox-invalid {
+		@each $variant in $variants {
+			&.ids-checkbox-#{$variant} {
+				input[type="checkbox"] {
+					border-color: var(--ids-comp-checkbox-input-unselected-color-border-error-enabled);
 
+					&:not(:disabled):hover {
+						border-color: var(--ids-comp-checkbox-input-unselected-color-border-error-hovered);
+					}
+
+					&:not(:disabled):active {
+						border-color: var(--ids-comp-checkbox-input-unselected-color-border-error-pressed);
+					}
+
+					&:not(:disabled):focus {
+						border-color: var(--ids-comp-checkbox-input-unselected-color-border-error-focused);
+					}
+
+					&:disabled {
+						border-color: var(--ids-comp-checkbox-input-unselected-color-border-error-disabled);
+					}
+				}
+
+				input[type="checkbox"]:checked, input[type="checkbox"]:indeterminate {
+					border-color: var(--ids-comp-checkbox-input-selected-color-border-error-enabled);
+
+					&:not(:disabled):hover {
+						border-color: var(--ids-comp-checkbox-input-selected-color-border-error-hovered);
+					}
+
+					&:not(:disabled):active {
+						border-color: var(--ids-comp-checkbox-input-selected-color-border-error-pressed);
+					}
+
+					&:not(:disabled):focus {
+						border-color: var(--ids-comp-checkbox-input-selected-color-border-error-focused);
+					}
+
+					&:disabled {
+						border-color: var(--ids-comp-checkbox-input-selected-color-border-error-disabled);
+					}
+				}
+			}
+		}
+
+		&.ids-checkbox-light {
+			input[type="checkbox"]:checked, input[type="checkbox"]:indeterminate {
+				& + .ids-checkbox__icon {
+					color: var(--ids-comp-checkbox-input-selected-color-fg-icon-error-enabled);
+				}
+
+				&:not(:disabled):hover + .ids-checkbox__icon {
+					color: var(--ids-comp-checkbox-input-selected-color-fg-icon-error-hovered);
+				}
+
+				&:not(:disabled):active + .ids-checkbox__icon {
+					color: var(--ids-comp-checkbox-input-selected-color-fg-icon-error-pressed);
+				}
+
+				&:not(:disabled):focus + .ids-checkbox__icon {
+					color: var(--ids-comp-checkbox-input-selected-color-fg-icon-error-focused);
+				}
+			}
+		}
+			
 	}
 
 	&.ids-checkbox-disabled {
