42af1379c9b5a6397e621b5b307f2b0fe0a5fa1e
Árpád Soós <arpad.soos@icellmobilsoft.hu>
2024-08-28 11:50:19 +0200

    [IDS]: added missing desc for props

--- src/components/button/models/IdsButtonProps.interface.ts
+++ src/components/button/models/IdsButtonProps.interface.ts
@@ -4,12 +4,42 @@ import { Component } from "vue";
 import { IdsButtonTypeAttributeType } from "@components/button/models/IdsButtonTypeAttribute.type";
 import { IdsButtonAppearanceType } from "@components/button/models/IdsButtonAppearance.type";
 
+/**
+ * Props for the IdsButton component
+ */
 export interface IdsButtonProps {
-  size?: SizeType,
-  disabled?: boolean,
-  leadingIcon?: Component,
-  trailingIcon?: Component,
-  variant?: AllVariantsType,
-  type?: IdsButtonTypeAttributeType,
-  appearance?: IdsButtonAppearanceType,
+  /**
+   * The size of the button component
+   */
+  size?: SizeType;
+
+  /**
+   * Determines if the button is disabled
+   */
+  disabled?: boolean;
+
+  /**
+   * The icon to display before the button text
+   */
+  leadingIcon?: Component;
+
+  /**
+   * The icon to display after the button text
+   */
+  trailingIcon?: Component;
+
+  /**
+   * The variant/style of the button
+   */
+  variant?: AllVariantsType;
+
+  /**
+   * The type attribute for the button (e.g., "button", "submit", "reset")
+   */
+  type?: IdsButtonTypeAttributeType;
+
+  /**
+   * The appearance style of the button
+   */
+  appearance?: IdsButtonAppearanceType;
 }
